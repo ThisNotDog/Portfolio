4dc0d53ba826b8a45f18bcd2f74e54afda334dcc
ThisNotDog <111262255+ThisNotDog@users.noreply.github.com>
2022-12-18 16:06:05 -0500

    Update word_cloud.py

--- word_cloud.py
+++ word_cloud.py
@@ -1,5 +1,11 @@
 import wordcloud
-y = open('reptiliandude.txt', 'r')
+
+#cleans a text file, filters out non \w characters
+#removes common words
+#ports to a set, counting instances of duplicate words
+#creates a wordcloud of the resulting set
+
+y = open(<'your text file'>, 'r')
 file_contents = y.read()
 
 def calculate_frequencies(file_contents):
@@ -11,10 +17,7 @@ def calculate_frequencies(file_contents):
         "their", "what", "which", "who", "whom", "this", "that", "am", "are", "was", "were", "be", "been", "being", \
         "have", "has", "had", "do", "does", "did", "but", "at", "by", "with", "from", "here", "when", "where", "how", \
         "all", "any", "both", "each", "few", "more", "some", "such", "no", "nor", "too", "very", "can", "will", "just"]
-    
     for i in file_contents:
-        if i in uninteresting_words:
-            pass
         if i.isalpha():
             iterated += i
         if i == " ":
@@ -23,19 +26,15 @@ def calculate_frequencies(file_contents):
             iterated += " "        
         else:
             pass
-    
     processed = iterated.lower()
     split = processed.split(" ")
-    
-    
     txt_set = {}
     delete_words = [k for k in split if k not in uninteresting_words]
     
     txt_set = {i:delete_words.count(i) for i in delete_words}
     for k in delete_words:
         if k in uninteresting_words:
-            del txt_set[k]
-    
+            del txt_set[k]   
     return txt_set
 print(calculate_frequencies(file_contents))
  
