ebd3b0fb60f5e45ab064d0d2cffca6ed0d36040f
Tyler Strate <tylerstrate@proton.me>
2023-01-04 20:10:15 -0500

    cleaned for presentation

--- word_cloud.py
+++ word_cloud.py
@@ -1,15 +1,14 @@
 import wordcloud
-
-#cleans a text file, filters out non \w characters
+#cleans a text file, filters out non alpha characters
 #removes common words
 #ports to a set, counting instances of duplicate words
 #creates a wordcloud of the resulting set
 
-y = open(<'your text file'>, 'r')
-file_contents = y.read()
+opened_file = open('<your text file>', 'r', encoding='utf-8')
+file_contents = opened_file.read()
 
-def calculate_frequencies(file_contents):
-    iterated = ""
+def calculate_frequencies():
+    filtered_characters = ""
     processed = ""
     punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
     uninteresting_words = ["the", "a", "to", "if", "is", "it", "of", "and", "or", "an", "as", "i", "me", "my", \
@@ -17,27 +16,26 @@ def calculate_frequencies(file_contents):
         "their", "what", "which", "who", "whom", "this", "that", "am", "are", "was", "were", "be", "been", "being", \
         "have", "has", "had", "do", "does", "did", "but", "at", "by", "with", "from", "here", "when", "where", "how", \
         "all", "any", "both", "each", "few", "more", "some", "such", "no", "nor", "too", "very", "can", "will", "just"]
-    for i in file_contents:
-        if i.isalpha():
-            iterated += i
-        if i == " ":
-            iterated += i
-        if i == ".":
-            iterated += " "        
+    for character in file_contents:
+        if character.isalpha():
+            filtered_characters += character
+        if character == " ":
+            filtered_characters+= character
+        if character in punctuations:
+            pass
         else:
             pass
-    processed = iterated.lower()
-    split = processed.split(" ")
+    lower_case_substrings = filtered_characters.lower()
+    substring_list = lower_case_substrings.split(" ")
     txt_set = {}
-    delete_words = [k for k in split if k not in uninteresting_words]
-    
-    txt_set = {i:delete_words.count(i) for i in delete_words}
-    for k in delete_words:
+    deleted_words = [word for word in substring_list if word not in uninteresting_words]   
+    txt_set = {i:deleted_words.count(i) for i in deleted_words}
+    for k in deleted_words:
         if k in uninteresting_words:
             del txt_set[k]   
     return txt_set
-print(calculate_frequencies(file_contents))
+print(calculate_frequencies())
  
 cloud = wordcloud.WordCloud()
-cloud.generate_from_frequencies(calculate_frequencies(file_contents))
+cloud.generate_from_frequencies(calculate_frequencies())
 cloud.to_file("frequencies.jpg")  
